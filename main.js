57e78d3200f9a3897736426a1fe0227490689ec9
CristianPérez <cristianperezcases@gmail.com>
2023-05-30 20:41:22 +0200

    feat second update

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-// Iniciamos la ejecución.
-let pokemons
+// Variable general
+
+let pokemons;
+
+// variable init para llamar a los pokemons
 
 const init = async () => {
     const globalDiv$$ = document.createElement('div');
     globalDiv$$.className = 'pokemons';
     document.body.appendChild(globalDiv$$);
     
-
-    //Iniciar la solicitud de información.
+  //Iniciar la solicitud de información.
   
     pokemons = await getPokemons();
     printPokemons(pokemons);
@@ -16,7 +18,7 @@ const init = async () => {
 
 const botonesHeader = document.querySelectorAll(".btn-header");
 const container$$ = document.createElement('div');
-const form = document.getElementById("search")
+const form = document.getElementById("search");
 
 //Bloque de peticiones.
 
@@ -35,23 +37,26 @@ async function getDetailPokemons(pokemons) {
     return Promise.all(pokemonsPromises);
 }
 
+// Bloque para pintar los pokemons
 
 const printPokemons = (pokemons) => {
-    container$$.id = "pokedex"
+    container$$.innerHTML = "";
+    container$$.id = "pokedex";
     container$$.className = 'pokemons';
           console.log(pokemons);
     for (const pokemon of pokemons) {
       const div$$ = document.createElement('div');
       div$$.className = 'flip-card';
-      div$$.classList.add("card")
+      div$$.classList.add("card");
 
+      // Cartas para hacer el flip
 
       const innerDiv$$ = document.createElement('div');
-      innerDiv$$.className = 'flip-card-inner'
+      innerDiv$$.className = 'flip-card-inner';
       const frontDiv$$ = document.createElement('div');
-      frontDiv$$.className = 'flip-card-front'
+      frontDiv$$.className = 'flip-card-front';
       const backDiv$$ = document.createElement('div');
-      backDiv$$.className = 'flip-card-back'
+      backDiv$$.className = 'flip-card-back';
       backDiv$$.classList.add(pokemon.types[0].type.name);
       
 
@@ -60,51 +65,54 @@ const printPokemons = (pokemons) => {
       const img$$ = document.createElement('img');
       img$$.setAttribute('src', pokemon.sprites.front_shiny);
       img$$.setAttribute('alt', pokemon.name);
-      img$$.classList.add("card-image")
+      img$$.classList.add("card-image");
   
       // Utilizo la misma propiedad pero le pongo el nombre del pokemon con un h2.
   
       const h2$$ = document.createElement('h2');
       h2$$.textContent = pokemon.name;
-      h2$$.classList.add("card-title")
+      h2$$.classList.add("card-title");
 
       // Creo las clases de los pokemons
-      const span$$ = document.createElement('span')
-      span$$.textContent = "#" + pokemon.id;
-      span$$.classList.add("id")
 
-      const p$$ = document.createElement("p")
+      const span$$ = document.createElement('span');
+      span$$.textContent = "#" + pokemon.id;
+      span$$.classList.add("id");
+
+      const p$$ = document.createElement("p");
    for (const pokemonType of pokemon.types) {
     
-      const span2$$ = document.createElement('span')
+      const span2$$ = document.createElement('span');
 
       span2$$.textContent = pokemonType.type.name;
 
       p$$.appendChild(span2$$);
 
-      span2$$.classList.add("card-subtitle", pokemonType.type.name) 
+      span2$$.classList.add("card-subtitle", pokemonType.type.name); 
    }
 
-   const h1$$ = document.createElement("h4")
-   h1$$.textContent = "stats"
-   h1$$.classList.add("backtitles")
-   const ul$$ = document.createElement("ul")
+    // Parte trasera de las tarjetas con estadisticas y habilidades
+
+   const h1$$ = document.createElement("h4");
+   h1$$.textContent = "stats";
+   h1$$.classList.add("backtitles");
+   const ul$$ = document.createElement("ul");
 for (const pokemonStats of pokemon.stats) {
 
-      const li$$ = document.createElement('li')
+      const li$$ = document.createElement('li');
 
       li$$.textContent = pokemonStats.stat.name + ": " + pokemonStats.base_stat; 
     
       ul$$.appendChild(li$$);
 
-      ul$$.classList.add("statslist")
+      ul$$.classList.add("statslist");
    }
   
-    const h4$$ = document.createElement("h4")
-    h4$$.textContent = "skills"
-    h4$$.classList.add("backtitles")
-   const skillsDiv$$ = document.createElement ("ul")
-    skillsDiv$$.classList.add("skills")
+    const h4$$ = document.createElement("h4");
+    h4$$.textContent = "skills";
+    h4$$.classList.add("backtitles");
+   const skillsDiv$$ = document.createElement ("ul");
+    skillsDiv$$.classList.add("skills");
    
         for (let i = 0; i < 4; i++){
 
@@ -112,7 +120,7 @@ for (const pokemonStats of pokemon.stats) {
 
             let skillname = pokemon.moves[number].move.name;
 
-            const li$$ = document.createElement('li')
+            const li$$ = document.createElement('li');
             
             li$$.textContent = skillname;
 
@@ -125,10 +133,10 @@ for (const pokemonStats of pokemon.stats) {
         skillp$$.appendChild(skillsDiv$$);
 
 
-      div$$.appendChild(innerDiv$$)
-      innerDiv$$.appendChild(frontDiv$$)
-      innerDiv$$.appendChild(backDiv$$)
-      frontDiv$$.appendChild(span$$) 
+      div$$.appendChild(innerDiv$$);
+      innerDiv$$.appendChild(frontDiv$$);
+      innerDiv$$.appendChild(backDiv$$);
+      frontDiv$$.appendChild(span$$);
       frontDiv$$.appendChild(img$$);
       frontDiv$$.appendChild(h2$$);
       frontDiv$$.appendChild(p$$);
@@ -138,25 +146,25 @@ for (const pokemonStats of pokemon.stats) {
       backDiv$$.appendChild(skillsDiv$$);
       container$$.appendChild(div$$);
     }
-  
+
     document.body.appendChild(container$$);
 };
   
+// Filtro por tipo de pokemons
+
 botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
   const botonId = event.currentTarget.id;
 
-  container$$.innerHTML = "";
-
   let filteredPokemons = pokemons.filter((pokemon) => {
    
-  let found = false  
+  let found = false;
 
     for (const type of pokemon.types) {
       if (found == false){
         found = (type.type.name == botonId);
       } 
     }
-    return found
+    return found;
   })
 
   if (botonId == "ver-todos"){
@@ -164,8 +172,23 @@ botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
   }
 
   printPokemons(filteredPokemons);
- 
+}));
 
-}))
+
+let searchPokemon = document.getElementById("searchButton");
+let searchInput = document.getElementById("search");
+
+searchPokemon.addEventListener("click", function() {
+  let searchedPokemon = searchInput.value;
+  search(searchedPokemon);
+});
+
+function search(searchedPokemon) {
+
+  let filteredPokemons = pokemons.filter((pokemon) => {
+    return pokemon.name.includes(searchedPokemon)
+  });
+  printPokemons(filteredPokemons);
+}
 
 init();
